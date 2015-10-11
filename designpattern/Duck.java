00f9953f2f5a2923f28730ffd6320164b350d983
155320754 <155320754@qq.com>
2015-10-11 09:41:24 +0800

    add ModelDuck and FlyRocketPowered.

--- designpattern/Duck.java
+++ designpattern/Duck.java
@@ -19,4 +19,11 @@ public abstract class Duck {
     public void swim() {
         System.out.println("All duck float, even decoys!");
     }
+
+    public void setFlyBehavior(FlyBehavior fb) {
+        flyBehavior fb;
+    }
+
+    public void setQuackBehavior(QuackBehavior qb) {
+        quackBehavior = qb;
 }
